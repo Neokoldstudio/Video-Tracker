8c51b2f091e6782d2fb616c8cc03317875d55f85
neokoldstudio <nightmare.vid@gmail.com>
2022-02-10 16:55:43 +0100

    test

---
+++ src/models/Scale.py
@@ -0,0 +1,9 @@
+class Scale:
+    def __init__(self, p1, p2, length):
+        self.start = p1
+        self.end = p2
+        self.length = length
+
+    def convCoord(self):
+        pass
+        
