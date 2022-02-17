eb36db4c8dfe1f2e289fc5aecc1fd111844146b1
neokoldstudio <nightmare.vid@gmail.com>
2022-02-17 16:54:18 +0100

    filerepo and tests

--- src/models/Point.py
+++ src/models/Point.py
@@ -1,8 +1,9 @@
 class Point:
     """point class : a class to store points data with getters and setters. ( include small log function )"""
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
+    def __init__(self,x,y,t):
+        self.__x = x
+        self.__y = y
+        self.__time = t
 
     def getX(self):
         """getter for the x value"""
@@ -11,21 +12,7 @@ class Point:
     def getY(self):
         """getter for the y value"""
         return self.y
-    
-    def setX(self, newX):
-        """setter for the x value, take a float as input"""
-        self.x = newX
 
-    def setY(self, newY):
-        """setter for the y value, take a float as input"""
-        self.y = newY
-
-    def Log(self):#optional
-        """quirky and silly little log function"""
-        print("({0}, {1})".format(self.x,self.y))
-    
-
-if __name__ == "__main__":
-    test = Point(300,200)
-    test.Log()
-    print(test.getX(), test.getY())
+    def getTime(self):
+        """getter for the y value"""
+        return self.time
